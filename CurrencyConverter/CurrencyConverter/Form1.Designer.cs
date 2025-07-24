447543a77852bc3fe27940339d03a43fb99445b7
Create99-sh <3504324576@qq.com>
2025-07-24 12:56:37 +0800

    Add files via upload
    Add Day1 Currency Converter

---
+++ CurrencyConverter/CurrencyConverter/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.cmbCurrency = new System.Windows.Forms.ComboBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(359, 40);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(229, 25);
+            this.txtAmount.TabIndex = 0;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(359, 172);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(229, 25);
+            this.txtResult.TabIndex = 0;
+            // 
+            // cmbCurrency
+            // 
+            this.cmbCurrency.FormattingEnabled = true;
+            this.cmbCurrency.Items.AddRange(new object[] {
+            "USD（美元）",
+            "EUR（欧元）",
+            "JPY（日元）"});
+            this.cmbCurrency.Location = new System.Drawing.Point(359, 107);
+            this.cmbCurrency.Name = "cmbCurrency";
+            this.cmbCurrency.Size = new System.Drawing.Size(209, 23);
+            this.cmbCurrency.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("宋体", 20F);
+            this.btnConvert.Location = new System.Drawing.Point(294, 263);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(213, 54);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "转换";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.Location = new System.Drawing.Point(267, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "金额";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F);
+            this.label2.Location = new System.Drawing.Point(267, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "币种";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F);
+            this.label3.Location = new System.Drawing.Point(267, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "结果";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.cmbCurrency);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtAmount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ComboBox cmbCurrency;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CurrencyConverter/CurrencyConverter/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //1.获取输入金额
+            double amount;
+            if (!double.TryParse(txtAmount.Text, out amount))
+            {
+                MessageBox.Show("请输入有效数字！");
+            }
+
+            //2.根据选择币种确定汇率
+            double rate = 0;
+            string currency = cmbCurrency.Text;
+
+            if (currency.Contains("USD")) rate = 6.88; //1美元=6.88人民币
+            else if (currency.Contains("EUR")) rate = 7.92; //1欧元=7.92人民币
+            else if (currency.Contains("JPY")) rate = 0.049; //1日元=0.049人民币
+
+            //3.计算并显示结果
+            double reasult = amount * rate;
+            txtResult.Text = reasult.ToString("0.00");//保留两位小数
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
